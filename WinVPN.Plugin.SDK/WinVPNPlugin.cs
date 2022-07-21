0e18cce3e28968082f086158c732d3ebb8724559
kali <kali@cqdasheng.com>
2022-07-21 18:14:27 +0800

    update plugin

--- WinVPN.Plugin.SDK/WinVPNPlugin.cs
+++ WinVPN.Plugin.SDK/WinVPNPlugin.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Input;
 
 namespace WinVPN.Plugin.SDK
 {
