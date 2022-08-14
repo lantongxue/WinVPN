1a7b1874ca91a81cc6523fe2c1e009c3e89620aa
kali <863448246@qq.com>
2022-08-14 20:47:26 +0800

    update

---
+++ WinVPN/Model/AppConfig.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinVPN.Model
+{
+    public class AppConfig : ObservableObject
+    {
+        private string dns1 = "";
+        private string dns2 = "";
+
+        public string Dns1
+        { 
+            get => dns1; 
+            set => SetProperty(ref dns1, value); 
+        }
+        public string Dns2 
+        { 
+            get => dns2; 
+            set => SetProperty(ref dns2, value); 
+        }
+    }
+}
